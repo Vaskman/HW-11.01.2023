32f9fa6770e27c8584d8474ea7671cb7ce0646e4
Vaskman <z.vaskman@gmail.com>
2023-01-11 18:33:03 +0300

    task 1-ok

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HW-11.01.2023");
+        task1();
     }
-}
+    //"Home Work 09.01.2023");
+    public static void task1() {
+        System.out.println(" Задача №1 ");
+        int clientOS =0;
+        if (clientOS==1){
+            System.out.println("Установите версию приложения для Android по ссылке");}
+        if(clientOS==0){
+            System.out.println("Установите версию приложения для iOS по ссылке");}
+        System.out.println(" Задача №2" );
+
+        }
+    }
